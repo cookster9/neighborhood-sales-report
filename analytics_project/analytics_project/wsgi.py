70f8371329d2f84660bc5fc43514e60c1ffd1441
Andrew Cook <andrewcook@Andrews-Air.attlocal.net>
2023-05-02 17:59:35 -0500

    tweaked wsgi.py to allow logging later

--- analytics_project/analytics_project/wsgi.py
+++ analytics_project/analytics_project/wsgi.py
@@ -13,4 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "analytics_project.settings")
 
-application = get_wsgi_application()
+def get_application():
+    return get_wsgi_application()
+
+application = get_application()
