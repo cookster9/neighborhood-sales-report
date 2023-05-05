7cf0588b43a6b6fd583464199e5aa84ce4d29aa0
Andrew Cook <andrewcook@Andrews-Air.attlocal.net>
2023-05-04 21:56:05 -0500

    Cleaned up views and urls

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard_with_pivot, name='dashboard_with_pivot'),
+    path('', views.map_rendered, name='map_rendered'),
     path('data', views.pivot_data, name='pivot_data'),
     path('map', views.interactive_map, name='interactive_map'),
-    path('mapiframe', views.map_rendered, name='map_rendered'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -10,18 +10,11 @@ import os
 
 module_dir = os.path.dirname(__file__)
 
-def dashboard_with_pivot(request):
-    return render(request, 'dashboard/map_homepage.html', {})
-
 def interactive_map(request):
     return render(request, 'dashboard/base-map.html', {})
 
 def map_rendered(request):
-    file_path = module_dir + '/static/dashboard/html/base-map.html' # '/templates/dashboard/base-map.html'
-    data_file = open(file_path, 'rb')  # I used'rb' here since I got an 'gbk' decode error
-    data = data_file.read().decode()
-    # map_html = Path('/dashboard/templates/dashboard/base-map.html').read_text()
-    context = {'my_map': data, "map_path": os.path.normpath(file_path)}
+    context = {}
     return render(request, 'dashboard/map_rendered.html', context)
 
 def pivot_data(request):
