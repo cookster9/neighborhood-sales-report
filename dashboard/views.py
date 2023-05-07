c51449376cc559d3791a3e20a5ad12d03c424b8a
Andrew Cook <andrewcook@Andrews-Air.attlocal.net>
2023-05-07 00:16:09 -0500

    added graph of recent sales

--- dashboard/views.py
+++ dashboard/views.py
@@ -10,13 +10,23 @@ import os
 
 module_dir = os.path.dirname(__file__)
 
+# /map
 def interactive_map(request):
     return render(request, 'dashboard/base-map.html', {})
 
+# /
 def map_rendered(request):
+    file_path = module_dir + '/static/dashboard/json/total_sales.json'  # '/templates/dashboard/base-map.html'
+    data_file = open(file_path, 'rb')  # I used'rb' here since I got an 'gbk' decode error
+    data = data_file.read().decode()
+    # map_html = Path('/dashboard/templates/dashboard/base-map.html').read_text()
+    print(data)
     context = {}
+    context["sales_data"] = json.loads(data)
+    print(context)
     return render(request, 'dashboard/map_rendered.html', context)
 
+# /data
 def pivot_data(request):
     dataset = list(RealEstateInfoScrape.objects.values("neighborhood").annotate(count=Count(id)))
     # data = serializers.serialize('json', dataset)
