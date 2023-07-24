ae5c65bff4bce00b3ba93f72561d98bf5be2a286
Andrew Cook <andrewcook@Andrews-Air.attlocal.net>
2023-07-23 20:07:22 -0500

    unnecessary setting

--- analytics_project/settings.py
+++ analytics_project/settings.py
@@ -89,8 +89,7 @@ DATABASES = {
         "NAME": config('DB_NAME'),
         "USER": config('DB_USER'),
         "PASSWORD": config('DB_PASSWORD'),
-        "HOST": config('DB_HOST'),
-        "CONNECT_TIMEOUT": 100
+        "HOST": config('DB_HOST')
     }
 }
 
