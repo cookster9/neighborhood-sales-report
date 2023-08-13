e465b5b9613163cf638d8772d40bba466fe8dbe0
Andrew Cook <andrewcook@Andrews-Air.attlocal.net>
2023-08-12 19:28:57 -0500

    SSL Redirect setting

--- analytics_project/settings.py
+++ analytics_project/settings.py
@@ -32,6 +32,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(",")
+SECURE_SSL_REDIRECT = config('SSL_REDIRECT')
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
