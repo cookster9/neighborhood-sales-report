d0be8933f2822f5235b72cd77e72b2bbe3f853f9
Andrew Cook <andrewcook@Andrews-Air.attlocal.net>
2023-08-13 16:06:05 -0500

    static root in env

--- analytics_project/settings.py
+++ analytics_project/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = config('STATIC_ROOT', default=os.path.join(BASE_DIR, 'static'))
 print(STATIC_ROOT)
 
 # Default primary key field type
